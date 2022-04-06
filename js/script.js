2c14ea507f420e6addecf31b3a74f256ef9e38fe
MbargouG <97864057+MbargouG@users.noreply.github.com>
2022-04-06 16:45:24 -0500

    adding js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+function init() {
+    function alertFunction() {
+        var txt = document.getElementById("entryinput").value;
+        alert("Mbargou Gueye: + txt");
+        document.getElementById("textoutput").innerHTML =txt
+    }
+    var button = document.getElementById('entrybutton');
+    button.addEventListener('click', alertFunction);
 
+}
+window.addEventListener('load', init);
