7c4536ef311ba8971f90ad4c57387772be39b3ad
MbargouG <97864057+MbargouG@users.noreply.github.com>
2022-04-06 17:23:16 -0500

    wrong placement

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function init() {
     //add your javascript between these two lines of code
-    function alertFunction() {
+    function alertFunction(){
         alert("Mbargou Gueye: ' + inputForm.value");
     }
     function setText(){
         var outputText = document.getElementById('textoutput');
         outputText.innerHTML = "<h2>" = inputForm.value + "</h2>";
     }
-    var alertDiv = document.getElementById('textoutput');
-    var inputForm = document.getElementById('entrybutton');
+    var alertDiv = document.getElementById('entrybutton');
+    var inputForm = document.getElementById('entryinput');
     alertDiv.addEventListener('click', alertFunction);
     alertDiv.addEventListener('click',setText);
 }
